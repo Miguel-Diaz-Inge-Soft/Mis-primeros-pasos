61b7ac7ebb7ff3d9ca68fbcccc531c9efd64a59b
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-22 07:32:13 -0600

    Add files via upload

---
+++ estructura de programacion secuancial 2.py
@@ -0,0 +1,5 @@
+precio=int(input("ingrese el precio del producto: "))
+cant=int(input("ingrese la cantidad de productos a llevar: "))
+pago=precio*cant
+print("la cantidad total a pagar por los productos es de: ")
+print(pago)

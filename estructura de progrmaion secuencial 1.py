8bf192c4b9f87a268c23c02998040c9613a8d9e9
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-22 07:23:00 -0600

    Add files via upload

---
+++ estructura de progrmaion secuencial 1.py
@@ -0,0 +1,8 @@
+num=int(input("ingrese el primer valor "))
+num2=int(input("ingrese segundo valor" ))
+suma=num+num2;
+producto=num*num2;
+print("la suma de los dos numeros es : ")
+print(suma)
+print("el producto de los dos numeros es : ")
+print(producto)

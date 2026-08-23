90349ac245e5ae9932692bd72a23183da1df31e2
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:12:52 -0600

    Update and rename estructuras condicionales simples y compuestas 4.py to Estructuras condicionales simples y compuestas 4.py
    Saber el promedio de 3 numeros ingresados

--- estructuras condicionales simples y compuestas 4.py
+++
@@ -1,8 +0,0 @@
-nu1=int(input("ingrese primer valor (rango del 1 al 10)"))
-nu2=int(input("ingrese segundo vaor(rango del 1 al 10)"))
-nu3=int(input("ingrese tercer valor (rango del 1 al 10)"))
-prom=(nu1+nu2+nu3)/3
-if prom>=7:
-    print("cursado")
-else:
-    print("no cursado")

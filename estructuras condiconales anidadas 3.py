5f288fe8a67ff77505eddf35a0c4d62abab167a8
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:20:41 -0600

    Add files via upload

---
+++ estructuras condiconales anidadas 3.py
@@ -0,0 +1,8 @@
+numero=int(input("ingrese el numero a analizar"))
+if numero>0:
+    print("el numero es positivo")
+else:
+    if numero<0:
+        print("el numero es negativo")
+    else:
+        print("el numero es nulo")

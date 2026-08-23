f2b0508765291e0281177ad5104e4944e2d55aba
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:06:06 -0600

    Add files via upload

---
+++ estructuras codicionales simples y compuestas 3.py
@@ -0,0 +1,11 @@
+nu1=int(input("ingrese primer valor "))
+nu2=int(input("ingrese segundo valor "))
+if nu1>nu2:
+    suma=nu1+nu2
+    res=nu1-nu2
+    print("la suma de los numeros es", suma , "la resta del mayor menos el segundo es :", res)
+else:
+    mul=nu1*nu2
+    div=nu2/nu1
+    print("el producto de los numeros es :",mul,"la divicion del segundo repecto el primero es :",div)
+    

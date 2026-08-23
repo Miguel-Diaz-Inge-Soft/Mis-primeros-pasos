7ae2fef529f145c9f0e0e9390c1ce6a85f6cd9a2
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:19:58 -0600

    Add files via upload

---
+++ estructuras condicionales anidadas 1 (2).py
@@ -0,0 +1,11 @@
+no1=int(input("ingrese primer nota del alumno"))
+no2=int(input("ingrese segunda nota del alumno"))
+no3=int(input("ingrese tercer nota del alumno"))
+prom=(no1+no2+no3)/3
+if prom>=7:
+    print("aprobado")
+else:
+    if prom>=4:
+        print("esta enla cuerda floja")
+    else:
+        print("esta reprobadisimo")

892d067fe9ea5e912357f0da915d50e979a6a7e0
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:20:20 -0600

    Add files via upload

---
+++ estructuras condicionales anidadas 2.py
@@ -0,0 +1,11 @@
+nu1=int(input("ingrese primer numero"))
+nu2=int(input("ingrese segundo numero
+nu3=int(input("ingrese tercer tercer
+if nu1>nu2:
+    print("el primer numero es mayor")
+else:
+    if nu2>nu3:
+        print("el segundo numero es mayor")
+    else:
+        print("el tercer numero es mayor")
+    

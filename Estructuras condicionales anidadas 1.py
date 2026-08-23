5530e741ebff326bf023e248168f0f0081016811
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-23 10:23:48 -0600

    Update and rename estructuras condicionales anidadas 1 (2).py to Estructuras condicionales anidadas 1.py
    Saber si un alumno aprobó mediante el ingreso de sus 3 notas y sacando su promedio

--- Estructuras condicionales anidadas 1.py
+++ Estructuras condicionales anidadas 1.py
@@ -1,6 +1,6 @@
-no1=int(input("ingrese primer nota del alumno"))
-no2=int(input("ingrese segunda nota del alumno"))
-no3=int(input("ingrese tercer nota del alumno"))
+no1=int(input("ingrese primer nota del alumno: "))
+no2=int(input("ingrese segunda nota del alumno: "))
+no3=int(input("ingrese tercer nota del alumno: "))
 prom=(no1+no2+no3)/3
 if prom>=7:
     print("aprobado")

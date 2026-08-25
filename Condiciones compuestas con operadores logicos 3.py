3d331b66c48069daffc716093bd0eb3c9d70d29a
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 21:02:23 -0600

    Add files via upload
    Determinar mediante valores ingresados, si la fecha corresponde a navidad

---
+++ Condiciones compuestas con operadores logicos 3.py
@@ -0,0 +1,9 @@
+dia=int(input("ingrese dia :"))
+mes=int(input("ingrese mes :"))
+año=int(input("ingrese año :"))
+
+if dia==24 and mes==12:
+    print("la fecha ingresada pertenece a navidad")
+else:
+    print("la fecha no pertence a navidad")
+    

4b44db0928f3bd5ee1d8232849d1accf66d35e5b
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 21:11:43 -0600

    Add files via upload
    Si los 3 números son iguales, sumar dos y el ultimo multiplicarlo por la suma

---
+++ Condiciones compuestas con operadores logicos 6.py
@@ -0,0 +1,8 @@
+nu1=int(input("ingresar primer valor :"))
+nu2=int(input("ngresar segundo valor :"))
+nu3=int(input("ingresar tercer valor :"))
+if nu1==nu2 and nu2==nu3:
+     suma=nu1+nu2
+     multi=suma*nu3
+     print("resultado de sumar el primer con el segundo numero y  multiplicado por el tercero :",multi)
+     

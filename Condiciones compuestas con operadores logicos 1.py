9daf3df185cc82c4a3a2d03cddf2007b0d81e2a6
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 20:58:28 -0600

    Add files via upload
    Saber que numero es mayor de 3

---
+++ Condiciones compuestas con operadores logicos 1.py
@@ -0,0 +1,11 @@
+nu1=int(input("ingrese primer numero"))
+nu2=int(input("ingrese segundo numero"))
+nu3=int(input("ingree tercer numero"))
+if nu1>nu2 and nu1>nu3:
+    print("el primer numero es el mayor")
+else:
+    if nu2>nu3:
+        print("el segundo numero es el mayor")
+    else:
+        print("el tercer numero es el mayor")
+               

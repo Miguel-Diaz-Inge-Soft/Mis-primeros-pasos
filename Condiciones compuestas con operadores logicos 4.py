fe21e1f72c2294fe60d08c2e512795dc7fac6ac3
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 21:04:17 -0600

    Add files via upload
    Determinar mediante  3 valores ingresados, si son mayores o menores a  10

---
+++ Condiciones compuestas con operadores logicos 4.py
@@ -0,0 +1,7 @@
+nu1=int(input("ingresar primer valor :"))
+nu2=int(input("ingresar segundo valor :"))
+nu3=int(input("ingresar tercer valor :"))
+if nu1<10 and nu2<10 and nu3<10:
+     print("los numeros son menores a 10")
+else:
+    print("los numeros no son menores a 10")

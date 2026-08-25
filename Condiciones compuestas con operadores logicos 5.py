adf5bf2ac38f8e30854f4ba5d5453b633bde1241
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 21:07:57 -0600

    Add files via upload
    Determinar de los 3 valores ingresados si hay algún número que sea menor a 10

---
+++ Condiciones compuestas con operadores logicos 5.py
@@ -0,0 +1,8 @@
+nu1=int(input("ingresar primer numero"))
+nu2=int(input("ingresar segundo numero"))
+nu3=int(input("ingresar tercer numero"))
+
+if nu1<10 or nu2<10 or nu3<10:
+    print("hay un numero ingresado que es menor a 10")
+else:
+    print("todos los numeros ingresados son mayores a 10")

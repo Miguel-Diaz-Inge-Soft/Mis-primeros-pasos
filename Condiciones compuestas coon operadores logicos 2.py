226a4637901936b920885f66938e50e3bf43e9c4
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-24 21:01:04 -0600

    Add files via upload
    Determinar mediante valores ingresados si la fecha corresponde al primer trimestre del año

---
+++ Condiciones compuestas coon operadores logicos 2.py
@@ -0,0 +1,8 @@
+dia=int(input("ingresar dia "))
+mes=int(input("ingresar mes "))
+año=int(input("ingresar año "))
+
+if mes==1 or mes==2 or mes==3:
+    print("la fecha corresponde al primer trimestre del año")
+else:
+    print("no corresponde al primer trimestre")

01bb40660e184becfaa93dd715d5c9e8c4f3c711
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-25 22:36:27 -0600

    Add files via upload
    Ingreso de 10 valores enteros, sacar la suma y el promedio de todos ellos

---
+++ Estructura repetitiva while 3.py
@@ -0,0 +1,10 @@
+x=1
+suma=0
+
+while x<=10:
+    valor=int(input("ingresar valor: "))
+    suma=suma+valor
+    x=x+1
+promedio=suma/10
+print("la suma de los 10 valores es: ",suma)
+print("el promedio de los valores es: ",promedio)

b582f1f1aec536352201e1166487c2cfcc645d36
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-26 21:53:37 -0600

    Add files via upload
    Secuencia de números de 11 en 11 desde el 11 hasta 251

---
+++ Estructura repetitiva while 8.py
@@ -0,0 +1,10 @@
+valor=int(input("ingresar 1 si desea continuar, ingresar 2 si desea cancelar"))
+
+if valor==1:
+    x=1
+    numero=1
+    while x<=25:
+        numero=numero+10
+        print(numero)
+        x=x+1
+        

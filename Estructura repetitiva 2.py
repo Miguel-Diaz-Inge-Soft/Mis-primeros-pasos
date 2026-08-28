2c74649ad8918154b2ab3edf811ec76cc35ab10e
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-27 22:15:43 -0600

    Add files via upload
    Ingreso de 10 valores entero, se imprime suma y promedio

---
+++ Estructura repetitiva 2.py
@@ -0,0 +1,8 @@
+suma=0
+for x in range(10):
+    valor=int(input("ingresar valor"))
+    suma=suma+valor
+
+promedio=suma/10
+print("suma de los valores es: ",suma)
+print("promedio de los valores: ",promedio)

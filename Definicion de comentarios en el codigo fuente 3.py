75b588daed3657a90f1421603350a9095dce0b2e
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-31 07:49:16 -0600

    Add files via upload
    Se comenta el código

---
+++ Definicion de comentarios en el codigo fuente 3.py
@@ -0,0 +1,11 @@
+suma=0 #iniciacion del contador suma en 0
+n=int(input("¿cantidad de valores a procesar?")) #cantidad de valores que se van ingresar
+#estructura repetitiva while
+for x in range(n):
+    valor=float(input("ingresar valor: "))
+    suma=suma+valor
+#impresion de la suma
+print("suma de los valores ingresados: ",suma)
+
+#python. A 17/08/2026. MIGUEL DIAZ
+

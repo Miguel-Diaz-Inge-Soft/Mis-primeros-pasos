119338d9bdea5d8899c4d4f6162a8debf16a8c95
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-08-31 07:48:36 -0600

    Add files via upload
    Se comenta el código

---
+++ Definicion de comentarios en el codigo fuente 2.py
@@ -0,0 +1,7 @@
+suma=0
+valor=int(input("ingresar valor, -1 finaliza la carga")) #se carga el primer valor
+while valor!=-1: #operacion con estrucura while
+    valor=int(input("ingresar valor -1, finaliza la carga"))
+    suma=suma+valor
+#impesion del texto
+print("suma de los valores: ",suma)

6498b657234f02e6a071bd1ea18e39237c5bfe1a
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-01 08:10:25 -0600

    Add files via upload
    Se ingresan 2 cadenas String y se hace un listado alfabético

---
+++ Variable enteras, flotantes y cadenas de caracteres 4.py
@@ -0,0 +1,9 @@
+nombre1=input("ingresar primer nombre: ")
+nombre2=input("ingresar segundo nombre: ")
+print("listado en orden alfabetico: ")
+if nombre1<nombre2:
+    print(nombre1)
+    print(nombre2)
+else:
+    print(nombre2)
+    print(nombre1)

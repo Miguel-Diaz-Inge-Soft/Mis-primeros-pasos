d5b10e5ec933db7b1bb25267a02d2da24647b76a
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-01 08:01:29 -0600

    Add files via upload
    Se ingresa por teclado valores de tipo String y se determina cual de ellos es mayor alfabéticamente

---
+++ Variables enteras, flotantes y cadenas de caracteres 2.py
@@ -0,0 +1,11 @@
+nombre1=input("ingrese nombre de la primer persona")
+nombre2=input("ingrese nombre de la segunda persona")
+if nombre1==nombre2:
+    print("ingresó dos nombres iguales")
+else:
+    if nombre1>nombre2:
+        print(nombre1)
+        print("es mayor alfabeticamente")
+    else:
+            print(nombre2)
+            print("es mayor alfabeticamente")

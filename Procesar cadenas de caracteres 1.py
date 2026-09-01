836ba97c2165be3eade9de8c8c5ed6afb4621019
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-01 08:12:32 -0600

    Add files via upload
    Se determina si una cadena String empieza con vocal o no

---
+++ Procesar cadenas de caracteres 1.py
@@ -0,0 +1,5 @@
+nombre=input("ingresar nombre: ")
+if nombre[0]== "a" or nombre[0]=="e" or nombre[0]=="i" or nombre[0]=="o" or nombre[0]=="u":
+    print("el nombre empieza con una vocal")
+else:
+    print("el nombre no empieza con vocal")

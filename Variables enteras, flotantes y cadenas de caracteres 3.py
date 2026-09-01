cd1b0b72c27ca7f32f6b35a31dfaeb5025ff74a4
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-01 08:07:02 -0600

    Add files via upload
    Suma de números N hasta que mediante con una cadena String se corte bucle while

---
+++ Variables enteras, flotantes y cadenas de caracteres 3.py
@@ -0,0 +1,8 @@
+suma=0
+opcion="si"
+while opcion=="si":
+    valor=int(input("ingrese numero a sumar"))
+    suma=suma+valor
+    opcion=input("¿desea continuar ingresando numeros? escriba si o no: ")
+print("suma de los valores ingresados")
+print(suma)

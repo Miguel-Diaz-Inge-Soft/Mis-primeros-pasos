9e0125e2e88bc10c471f58e793c9f584f88b6009
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-06 08:32:23 -0600

    Add files via upload

---
+++ Estructura de datos tipo lista 1.py
@@ -0,0 +1,9 @@
+lista=[10,20,30,40,50]
+suma=0
+x=0
+while x<len(lista):
+    suma=suma+lista[x]
+    x=x+1
+
+print("los elementos de la lista: ",lista)
+print("suma de todos los elementos: ",suma)

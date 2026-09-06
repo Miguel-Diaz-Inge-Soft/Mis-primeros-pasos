9717d6dc1ac4003b7d5e2b7f2e62a0e5fb467bd0
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-06 08:26:05 -0600

    Add files via upload

---
+++ Procesar cadenas de caracteres 5.py
@@ -0,0 +1,11 @@
+oracion=input("ingresar oración: ")
+oracion1=oracion.upper()
+x=0
+voc=0
+
+while x<len(oracion1):
+    if oracion[x]=="a" or oracion[x]=="e" or oracion[x]=="i" or oracion[x]=="o" or oracion[x]=="u":
+        voc=voc+1
+    x=x+1
+
+print("cantidad de vocales que hay en la oracion: ",voc)

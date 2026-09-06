e7110dc1eeded1bdc99d1a4eb3c0774cad02cd91
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-06 08:29:38 -0600

    Add files via upload

---
+++ Procesar cadenas de caracteres 6.py
@@ -0,0 +1,7 @@
+oracion=input("ingresar oracion: ")
+if len(oracion)>10 and len(oracion)<20:
+    print("datos váidos")
+    print(len(oracion))
+else:
+    print("datos no validos ajustar el numero de caracteres")
+    print(len(oracion))

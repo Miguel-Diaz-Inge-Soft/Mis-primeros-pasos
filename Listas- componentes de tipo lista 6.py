993dc40ed9cbfb7e7b44efe07ca6b75fbe93137f
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-14 23:58:23 -0600

    Add files via upload

---
+++ Listas- componentes de tipo lista 6.py
@@ -0,0 +1,14 @@
+
+#Se crea una lista por asignación con la cantidad de elementos
+#deseada, luego se imprime el último elemento de la lista principal
+
+lista= [[3,4,5,8,4,],[27,78],[4,7,8],[90,87,65],[2,45,789,4]]
+
+#Primer método de hacerlo
+print(lista[4])
+
+#Segundo método para hacerlo
+print(lista[len(lista)-1])
+
+#Tercer método para hacerlo
+print(lista[-1])

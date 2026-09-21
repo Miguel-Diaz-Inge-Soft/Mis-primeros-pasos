ae83539afab066d5fd8727b2ca59d9b0a348c253
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-20 22:07:58 -0600

    Add files via upload
    Se desarrolla un programa con dos funciones, la primera solicita el ingreso de un entero y muestra el cuadrado de dicho valor,  la segunda función solicita la carga de dos valores y muestra el producto de los mismos. Llamar desde el bloque el programa principal a ambas funciones

---
+++ Concepto de funciones- programación estructurada 3.py
@@ -0,0 +1,15 @@
+def cuadrado():
+    valor1=int(input("Ingresar valor para sacar su cuadrado: "))
+    valor2=valor1*valor1
+    print(f"El cuadrado del valor es: {valor2} ")
+
+def multi():
+    valor1=int(input("ingresar primer valor: "))
+    valor2=int(input("Ingresar segundo valor: "))
+    multipli=valor1*valor2
+    print(f"La multiplicación de los números es: {multipli}")
+
+    
+#Principal
+cuadrado()
+multi()

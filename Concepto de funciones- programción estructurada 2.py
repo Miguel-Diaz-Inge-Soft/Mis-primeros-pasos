2d5c4772351b4118d3fcccf0236c3eb296ab8ed0
Miguel-Diaz-Inge-Soft <miguelangeldiazmedina@gmail.com>
2026-09-20 22:05:14 -0600

    Add files via upload
    Se confecciona una aplicación que solicita la carga de dos valores y muestre su suma. Se repite la carga e impresión de a suma 5 veces

---
+++ Concepto de funciones- programción estructurada 2.py
@@ -0,0 +1,11 @@
+def carga_suma():
+    valor1=int(input("Ingrese el primer valor: "))
+    valor2=int(input("Ingrese el segundo valor: "))
+    suma=valor1+valor2
+    print(f"Suma de los valores: {suma}")
+    print("------------------------------------------------------")
+
+
+#Main
+for x in range(5):
+    carga_suma()
